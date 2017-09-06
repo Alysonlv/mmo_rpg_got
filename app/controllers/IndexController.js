acb325b33ac057d1995e173eb60a7c8e0b0b9486
Alysonlv <alysonlv@gmail.com>
2017-09-05 23:17:29 -0300

    final da parte do jogo

--- app/controllers/IndexController.js
+++ app/controllers/IndexController.js
@@ -1,3 +1,24 @@
 module.exports.home = function(application, req, res) {
-    res.render('index');
+    res.render('index', {validacao: {}});
+};
+
+module.exports.autenticar = function(application, req, res) {
+    var dadosForm = req.body;
+
+    req.assert('usuario', 'Usuário não foi informado').notEmpty();
+    req.assert('senha', 'Senha não foi informado').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if (erros) {
+        console.log('erossss = ' + erros);
+        res.render('index', {validacao: erros});
+        return;
+    }
+
+    var connection = application.config.dbConnection;
+    var usuariosDAO = new application.app.models.UsuariosDAO(connection);
+    usuariosDAO.autenticar(dadosForm, req, res);
+
+    //res.send("tudo certo");
 };
--- app/controllers/JogoController.js
+++ app/controllers/JogoController.js
@@ -1,3 +1,58 @@
 module.exports.jogo = function(application, req, res) {
-    res.render('jogo');
+    if (req.session.autorizado !== true) {
+        res.send('É necessário se logar!');
+        return;
+    }
+
+    var comando_invalido = 'n';
+
+    if (req.query.comando_invalido == 'yes') {
+        comando_invalido = 's';
+    }
+
+    var connection = application.config.dbConnection;
+    var jogoDAO = new application.app.models.JogoDAO(connection);
+    jogoDAO.iniciaJogo(res, req.session.usuario, req.session.casa, comando_invalido);
+
+    
+};
+
+module.exports.sair = function(application, req, res) {
+    
+    req.session.destroy(function(err) {
+        res.render('index', {validacao:{}})
+    });
+    
+};
+
+module.exports.suditos = function(application, req, res) {
+    if (req.session.autorizado !== true) {
+        res.send('É necessário se logar!');
+        return
+    }
+    res.render('aldeoes', {validacao:{}});
+};
+
+module.exports.pergaminhos = function(application, req, res) {
+    if (req.session.autorizado !== true) {
+        res.send('É necessário se logar!');
+        return
+    }
+    res.render('pergaminhos', {validacao:{}});
+};
+
+module.exports.ordenar_acao_sudito = function(application, req, res) {
+    var dadosForm = req.body;
+
+    req.assert('acao', "Ação deve ser informada").notEmpty();
+    req.assert('quantidade', "Quantidade deve ser informada").notEmpty();
+
+    var erros = req.validationErrors();
+
+    if (erros) {
+        res.redirect('jogo?comando_invalido=yes');
+        return;
+    }
+    
+    res.send('tudo ok');
 };
